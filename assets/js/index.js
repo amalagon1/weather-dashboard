059b5d70189483f54df77e0ac49232ed6a658b82
Andres Malagon <andresmalagon@Andress-MacBook-Pro.local>
2022-03-22 20:32:56 -0400

    current weather almost complete, added cityName

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@
 var button = document.querySelector(".button");
 var cityName = document.querySelector('.inputValue');
 var apiKey = "5686c46a54707e23e5474e6b38ba9744";
+var currentPic = document.getElementById("current-icon");
 
 // current weather conditions url
 
@@ -9,6 +10,12 @@ const currentUrl = "https://api.openweathermap.org/data/2.5/weather?q="
 const forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="
 
 
+//dynamically change city name
+function title() {
+    $("#title").text(cityName.value);
+}
+
+// }
 // function for both api calls
 function getData() {
     fetch(currentUrl + cityName.value + "&Appid=" + apiKey + "&units=imperial")
@@ -22,19 +29,31 @@ function getData() {
                 .then(data => {
                     console.log(data)
                 })
-            var tempValue = data['main']['temp'];
-            console.log(tempValue);
-            $("#current-temp").text(tempValue)
+
+            //extract data for current weather conditions
+            var temp = data['main']['temp'];
+            var humid = data['main']['humidity'];
+            var wind = data['wind']['speed'];
+            var icon = data.weather[0].icon;
+
+            $("#current-temp").text(temp);
+            $("#current-humidity").text(humid);
+            $("#current-wind").text(wind);
+            // $("#current-icon").text("https://openweathermap.org/img/w/" + icon + ".png");
+            // $(".current-icon").attr("src", "https://openweathermap.org/img/w/" + icon + ".png");
+            // ask tutor about setAttribute with jquery
+            currentPic.setAttribute("src", "https://openweathermap.org/img/w/" + icon + ".png");
+
 
 
         })
 
-
+    title(cityName)
 
 }
 
+
+
+
 // function triggering api call when submit button is clicked
 button.addEventListener("click", getData)
-
-
-
