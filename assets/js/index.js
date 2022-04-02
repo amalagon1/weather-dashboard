d04d6019a0e1d3ffc96788586747f56e5e61eb66
Andres Malagon <andresmalagon@Andress-MacBook-Pro.local>
2022-04-02 12:32:31 -0400

    current weather + started forecast

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ var button = document.querySelector(".button");
 var cityName = document.querySelector('.inputValue');
 var apiKey = "5686c46a54707e23e5474e6b38ba9744";
 var currentPic = document.getElementById("current-icon");
+const forecastEl = document.getElementById("forecast");
 
 // current weather conditions url
 
@@ -12,7 +13,11 @@ const forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="
 
 //dynamically change city name
 function title() {
+    // let date = new Date(data.dt * 1000)
+    // let dateStr = date.toLocaleDateString("en-US");
     $("#title").text(cityName.value);
+    // console.log(dateStr);
+
 }
 
 // }
@@ -35,18 +40,55 @@ function getData() {
             var humid = data['main']['humidity'];
             var wind = data['wind']['speed'];
             var icon = data.weather[0].icon;
+            //get date
+            let date = new Date(data.dt * 1000).toLocaleDateString("en-US")
 
+            $("#currentDate").text(date);
             $("#current-temp").text(temp);
             $("#current-humidity").text(humid);
             $("#current-wind").text(wind);
+
             // $("#current-icon").text("https://openweathermap.org/img/w/" + icon + ".png");
             // $(".current-icon").attr("src", "https://openweathermap.org/img/w/" + icon + ".png");
             // ask tutor about setAttribute with jquery
             currentPic.setAttribute("src", "https://openweathermap.org/img/w/" + icon + ".png");
 
 
+            console.log(date)
+
+            // display 5-day forecast
+
+            //added lat and lon to pass into next function
+            displayForecast(data);
+
 
         })
+
+    function displayForecast(data) {
+        console.log("DISPLAY FORECAST", data)
+
+        let forecastContainer = document.getElementById("forecast");
+        forecastContainer.innerHTML = ""
+
+        for (let i = 1; i <= 5; i++) {
+            let card = document.createElement("div");
+            card.classList = "card mb-r me-2";
+            card.style = "max-width: 12rem";
+
+            let cardBody = document.createElement("div");
+            cardBody.classList = "card-body"
+
+            // let iconImg = document.createElement('img')
+            // iconImg.setAttribute('src', `http://openweathermap.org/img/w/${data[i].weather[0].icon}.png`)
+            // cardBody.appendChild(iconImg);
+
+            let tempText = document.createElement("p")
+            tempText.classList = "card-text"
+            tempText.innerHTML = `Temp: ${data[i].daily.temp}°F`
+            cardBody.appendChild(tempText)
+
+        }
+    }
 
     title(cityName)
 
